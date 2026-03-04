d0908355bd6db2267ca60df342f0ab568c9ebe8e
raphafouanii <raphaelfouani@gmail.com>
2026-03-04 19:40:47 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,8 @@
-/* 
-    Variável - Pedacinho de memória
-    que eu posso guardar o que eu quiser
 
-    Função - Pedacinho de código QUE só EXECUTA
-    Quando é chamado
-
-    Algoritmo - Receita do Bolo
-    Lógica de Programação -  Fazer o bolo
-
-    // Algoritmo do nosso sistema
-    // Lógica de programação
-
-    [x] Saber quem é o botão
-    [x] Saber quando o botão foi clicado
-    [x] Saber quem é o textarea  
-    [x] Pegar o que tem dentro dele
-    [x] Enviar para a IA
-    [x] Pegar a resposta da IA e colocar na tela 
-    [x] Estilizar a resposta     
-
-    // Ir no HTML e pegar o botão
-    // HTML = document (documento)
-    // Selecionar (querySelector)
-    // Quem ? Botão
-    // Apelido para o botão - classes(class) = .
-    fetch - ferramenta do JS para se comunicar com o servidor
-*/
-
-// Descobri que é o botao
 let botao = document.querySelector(".botao-gerar")
 let endereco = "https://api.groq.com/openai/v1/chat/completions"
 
-// Criei a funcao que será chamada quando clicar 
-// no botao
+
 
 async function gerarCodigo() {
 
@@ -69,12 +39,9 @@ async function gerarCodigo() {
 
 }
 
-// ficar de olho no botao, quando clicado chamar o gerarCodigo
+
 botao.addEventListener("click", gerarCodigo)
 
 
-// vizinho curioso (addEventListener)
-// adicionar ouvinte de eventos
-// Evento = clique, digitei...
 
 
